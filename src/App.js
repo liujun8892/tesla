ca11b7a0d04399a02acf8e583116dbedcd18f53c
liujun <ajun_8892@163.com>
2022-01-09 16:06:37 +0800

    ADD: Section for tesla

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import Header from './components/Header'
+import Home from './components/Home'
+import Section from './components/Section'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header />
+      <Home /> */}
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpeg"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpeg"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpeg"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpeg"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        title="Solar Panels"
+        description="Lowest Cost Solar Panels in Americay"
+        backgroundImg="solar.jpeg"
+        leftBtnText="order-now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Solar Roof"
+        description="Produce Clean Energy From Your Roof"
+        backgroundImg="solar-roof.jpeg"
+        leftBtnText="order-now"
+        rightBtnText="Learn More"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpeg"
+        leftBtnText="Shop Now"
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+  return <Container>home</Container>
+}
+
+const Container = styled.div`
+  height: 100vh;
+`
+
+export default Home
